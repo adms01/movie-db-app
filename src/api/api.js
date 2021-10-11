85b27a446042c0356fffe6b25a338885b7224ed9
hali864 <48163863+hali864@users.noreply.github.com>
2021-10-11 22:12:25 +0100

    added useEffect cleanup

--- src/api/api.js
+++ src/api/api.js
@@ -4,20 +4,25 @@ export const api = axios.create({
   baseURL: process.env.REACT_APP_API_DOMAIN,
 });
 
-export const getMostRecentMovies = async ({ page }) => {
+export const getMostRecentMovies = async ({ page }, setError, source) => {
   try {
-    const res = await api.get(`/movie/upcoming?query=recent&api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}&page=${page}`);
+    const res = await api.get(`/movie/upcoming?query=recent&api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}&page=${page}`, {
+      cancelToken: source.token,
+    });
     return res.data;
   } catch (err) {
-    console.log(err);
+    setError(err);
   }
 };
 
-export const getMovieBySearch = async (query, page) => {
+export const getMovieBySearch = async (query, page, setError, source) => {
   try {
-    const res = await api.get(`/search/collection?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}&query=${query}&page=${page}`);
+    const res = await api.get(`/search/collection?api_key=${process.env.REACT_APP_MOVIE_DB_API_KEY}&query=${query}&page=${page}`, {
+      cancelToken: source.token,
+    });
     return res.data;
   } catch (err) {
     console.log(err);
+    setError(err);
   }
 };
--- src/components/App.js
+++ src/components/App.js
@@ -3,25 +3,38 @@ import styled from "styled-components";
 import Header from "./Header";
 import MovieFeed from "./MovieFeed";
 import { getMostRecentMovies, getMovieBySearch } from "../api/api";
+import axios from "axios";
 
 const App = () => {
   const [page, setPage] = useState(1);
   const [movies, setMovies] = useState([]);
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState();
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    let source = axios.CancelToken.source();
+
     (async () => {
-      const data = await getMostRecentMovies(page);
+      const data = await getMostRecentMovies(page, setError, source);
       setMovies(data.results);
     })();
-  }, []);
+
+    return () => {
+      source.cancel();
+    };
+  }, [page]);
 
   useEffect(() => {
+    let source = axios.CancelToken.source();
     (async () => {
-      const data = await getMovieBySearch(query, page);
+      const data = await getMovieBySearch(query, page, setError, source);
       setResults(data?.results);
     })();
+
+    return () => {
+      source.cancel();
+    };
   }, [query]);
 
   return (
